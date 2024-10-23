6f0c27201514c35d44b6bcee80aaeef4366b2422
claireisrael <israelclaire5@gmail.com>
2024-10-23 11:07:58 +0300

    using the recursion method

--- fibonacci_module.py
+++ fibonacci_module.py
@@ -1,15 +1,6 @@
 # CLAIRE and MELISSA
 #These are serires of numbers where by each number is the sum of the two proceeding numbers
 #0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, and so on
-# def fibonacci(n):
-#     if n == 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-#     else:
-#         return fibonacci(n - 1) + fibonacci(n - 2)
-    
-
 
 def fibonacci(n):
     if n < 0:
